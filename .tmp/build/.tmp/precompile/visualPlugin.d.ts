831d2a10ffba37978e39506bf12245de96732265
Christin Koshy <christinkoshy1998@gmail.com>
2024-01-24 16:38:26 +0530

    deleted\

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var EximTableViz9019D4275A2B4124B398FEE443B05F9F_DEBUG: IVisualPlugin;
-export default EximTableViz9019D4275A2B4124B398FEE443B05F9F_DEBUG;
